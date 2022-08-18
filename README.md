12197c0e8b9181ab135f9872885a3bdf123cefb7
liuliyi <liuliyi@shgbit.com>
2022-08-18 15:21:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: Sapphire Liu
+ * @Date: 2022-08-18 14:54:37
+ * @LastEditors: Sapphire Liu
+ * @LastEditTime: 2022-08-18 14:54:50
+ * @Description: file content...
+-->
+# sapphire611.js
+
+> only used for sapphire611
---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Sapphire Liu
+ * @Date: 2022-08-18 14:52:01
+ * @LastEditors: Sapphire Liu
+ * @LastEditTime: 2022-08-18 14:52:26
+ * @Description: test module for sapphire611.js..
+ */
+
+const test = () => {
+  console.log("test 4 sapphire611!");
+  const a = 1;
+  const b = 1;
+  console.log("a + b = " + (a + b));
+};
+
+module.exports = {
+  test,
+};
