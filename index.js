479a204b23377318fd67b30c03253ac71bb5fc5f
liuliyi <liuliyi@shgbit.com>
2022-08-18 16:42:07 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: Sapphire Liu
  * @Date: 2022-08-18 14:52:01
  * @LastEditors: Sapphire Liu
- * @LastEditTime: 2022-08-18 14:52:26
+ * @LastEditTime: 2022-08-18 16:41:59
  * @Description: test module for sapphire611.js..
  */
 
@@ -10,7 +10,9 @@ const test = () => {
   console.log("test 4 sapphire611!");
   const a = 1;
   const b = 1;
-  console.log("a + b = " + (a + b));
+  const c = a + b;
+  console.log("a + b = " + (c));
+  return c;
 };
 
 module.exports = {
